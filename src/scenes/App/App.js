aa0de7ab390ceec6c4058bab1959017351f027a7
joao.fernandes <joao.fernandes@blip.pt>
2017-09-04 14:25:20 +0100

    Restruct

--- src/scenes/App/App.js
+++ src/scenes/App/App.js
@@ -1,36 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from '../Header/Header';
-import Typed from 'typed.js';
-import _ from 'lodash';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "components/Header/Header";
+import Typed from "typed.js";
+import _ from "lodash";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       collapseOn: 1000
-    }
+    };
   }
 
   updateCollapseOnCoordinate() {
     this.setState({
       collapseOn: this.services.getBoundingClientRect().top
-    })
+    });
   }
 
   componentDidMount() {
     const typedOptions = {
-      strings: ['idea', 'product', 'service', 'app', 'team'],
+      strings: ["idea", "product", "service", "app", "team"],
       typeSpeed: 60,
       fadeOut: true,
-      fadeOutClass: 'typed-fade-out',
+      fadeOutClass: "typed-fade-out",
       fadeOutDelay: 200,
       loop: true
     };
-    this.typedText = new Typed('.typed-text', typedOptions);
+    this.typedText = new Typed(".typed-text", typedOptions);
 
-    document.addEventListener('scroll', _.throttle(this.updateCollapseOnCoordinate, 200).bind(this));
+    document.addEventListener(
+      "scroll",
+      _.throttle(this.updateCollapseOnCoordinate, 200).bind(this)
+    );
   }
 
   componentWillUnmount() {
@@ -43,23 +45,23 @@ class App extends Component {
         <Header collapseOn={this.state.collapseOn} />
         <section className="home">
           <div className="home-container">
-            <div className="home-illustration">
-            </div>
+            <div className="home-illustration" />
             <div className="home-content">
               <div className="home-copy">
                 <p className="copy-paragraph">We help you</p>
                 <p className="copy-paragraph">create value</p>
                 <p className="copy-paragraph">with your</p>
-                <span className="typed-text"></span>
+                <span className="typed-text" />
               </div>
             </div>
           </div>
         </section>
         <section
           className="services"
-          ref={(services) => { this.services = services; }}
-        >
-        </section>
+          ref={services => {
+            this.services = services;
+          }}
+        />
       </div>
     );
   }
